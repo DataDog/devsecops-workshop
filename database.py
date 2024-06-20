1719c033332ca39a4363ec75007406dd7e71a533
Julien Delange <julien.delange@datadoghq.com>
2024-06-19 20:36:50 -0400

    Update database.py

--- database.py
+++ database.py
@@ -32,6 +32,6 @@ def add_product(db_connection, product: Product):
     # catch specific exceptions.
     # https://docs.datadoghq.com/code_analysis/static_analysis_rules/python-best-practices/no-bare-except/
     # Replace with the following: 
-    # # except sqlite3.IntegrityError:
+    # except sqlite3.IntegrityError:
     except:
         return False
